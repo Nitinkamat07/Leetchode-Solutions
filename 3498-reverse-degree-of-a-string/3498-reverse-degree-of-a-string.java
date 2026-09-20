4b37bcf4481070914b958befaffc992840fce1e9
Nitin <168371071+Nitinkamat07@users.noreply.github.com>
2026-09-20 11:54:50 +0530

    Time: 1 ms (100%), Space: 44.2 MB (18.31%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int sum = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            // Reverse alphabet position:
+            // a = 26, b = 25, ..., z = 1
+            int reversePosition = 'z' - s.charAt(i) + 1;
+
+            // Position in string is i + 1
+            sum += reversePosition * (i + 1);
+        }
+
+        return sum;
+    }
+}
